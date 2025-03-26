c63e503252c717a52d67c585c0643be1c0f92652
Ana Lorenzo <ana.lorenzorivero@plexus.es>
2025-03-26 14:02:20 +0100

    agregados los imports y declarations en los test

--- src/app/core/services/user.service.spec.ts
+++ src/app/core/services/user.service.spec.ts
@@ -1,13 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { AuthService } from './user.service';
+import { UserService } from './user.service';
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe('UserService', () => {
+  let service: UserService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    service = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
